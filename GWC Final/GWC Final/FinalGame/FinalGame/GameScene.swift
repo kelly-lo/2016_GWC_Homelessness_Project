de7d3facc8cf62975f65087b7b9cbbea91744af9
Darya Verzhbinsky <darya99@gmail.comdarya99@gmail.com'dsdz>
2016-07-29 15:14:10 -0700

    Updated final project files

--- GWC Final/GWC Final/FinalGame/FinalGame/GameScene.swift
+++ GWC Final/GWC Final/FinalGame/FinalGame/GameScene.swift
@@ -1,45 +1,171 @@
 //
 //  GameScene.swift
-//  FinalGame
+//  game4
 //
 //  Created by Facebook on 7/27/16.
-//  Copyright (c) 2016 Katie. All rights reserved.
+//  Copyright (c) 2016 DaryaVer. All rights reserved.
 //
+
+
+
+//        let location = touch.locationInNode(self)
+//
+//                        if manSprite.containsPoint(location) {
+//                            self.score += 1
+//                            self.myLabel.text = String(self.score)
+//                        }
+//                    }
+
+
+
 
 import SpriteKit
 
+import UIKit
+
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+    
+    var TextureAtlas1 = SKTextureAtlas()
+    
+    var TextureArray = [SKTexture]()
+    
+    var People = SKNode()
+    
+    var moveAndRemove = SKAction()
+    
+    var gameStarted = Bool()
+    
+    var ScoreLabel = SKLabelNode(fontNamed: "Arial")
+    
+    var person = SKSpriteNode()
+    
+    var score:Int = 0
+    
+    func createPeople() {
         
-        self.addChild(myLabel)
+        People = SKNode()
+        
+        TextureAtlas1 = SKTextureAtlas(named: "people")
+        
+        for i in 1...TextureAtlas1.textureNames.count{
+            
+            let Name = "walkingman_\(i).png"
+            TextureArray.append(SKTexture(imageNamed: Name))
+        }
+        
+        person = SKSpriteNode(imageNamed: TextureAtlas1.textureNames[0])
+        person.position = CGPointMake(425,333.5)
+        person.size = CGSize(width: 100, height: 200)
+        
+        People.addChild(person)
+        
+        People.runAction(moveAndRemove)
+        
+        self.addChild(People)
+        
     }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
+    override func didMoveToView(view: SKView) {
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
+        //        self.backgroundColor = SKColor.magentaColor()
+        
+        self.ScoreLabel.text = String(self.score)
+        
+        self.ScoreLabel.fontSize = 20
+        self.ScoreLabel.position = CGPointMake(self.frame.width - 20, self.frame.height - 20)
+        self.addChild(ScoreLabel)
+    }
+    
+        
+        //
+        //        self.addChild(myLabel)
+        //        self.addChild(manSprite)
+        //        self.addChild(manSprite2)
+        //
+        //        self.manSprite.position = CGPointMake(425,333.5)
+        //        self.manSprite2.position = CGPointMake(425, 333.5)
+        //
+        //        self.manSprite.size = CGSize(width: 50, height: 100)
+        //        self.manSprite2.size = CGSize(width: 50, height: 150)
+        //
+        //        let moveAction = SKAction.moveToX(-25, duration: 3)
+        //        let moveAction2 = SKAction.moveToX(-25, duration: 5)
+        //
+        //        self.manSprite.runAction(moveAction)
+        //        self.manSprite2.runAction(moveAction2)
+        
+    //
+    /* Called when a touch begins */
+    //
+    //        for touch in touches {
+    //            let location = touch.locationInNode(self)
+    //
+    //            if manSprite.containsPoint(location) {
+    //                self.score += 1
+    //                self.myLabel.text = String(self.score)
+    //            }
+    //        }
+    
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        if gameStarted == false{
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            gameStarted = true
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
+
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+            let spawn = SKAction.runBlock({
+                () in
+                
+                self.createPeople()
+                self.person.runAction(SKAction.repeatActionForever(SKAction.animateWithTextures(self.TextureArray, timePerFrame: 0.2)))
+                
+            })
             
-            sprite.runAction(SKAction.repeatActionForever(action))
+            let delay = SKAction.waitForDuration(3.0)
             
-            self.addChild(sprite)
+            let SpawnDelay = SKAction.sequence([spawn, delay])
+            
+            let SpawnDelayForever = SKAction.repeatActionForever(SpawnDelay)
+            
+            self.runAction(SpawnDelayForever)
+            
+            let distance = CGFloat(self.frame.width + People.frame.width + 80)
+            
+            let movePeople = SKAction.moveByX(-distance, y: 0, duration: NSTimeInterval(0.01 * distance))
+            
+            let removePeople = SKAction.removeFromParent()
+            
+            moveAndRemove = SKAction.sequence([movePeople, removePeople])
+            
         }
+            
+        else {
+            
+            for touch in touches {
+                
+                let location = touch.locationInNode(self)
+                
+                if People.containsPoint(location) {
+                    self.score += 1
+                    self.ScoreLabel.text = String(self.score)
+                }
+            }
+        }
+        
     }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
-    }
+    
+    
+    
+    
+    //    override func update(currentTime: CFTimeInterval) {
+    //        /* Called before each frame is rendered */
+    //    }
 }
+
+
+
+
+
+
---
+++ GWC Final/GWC Final/FinalGame/FinalGame/RandomFunction.swift
@@ -0,0 +1,26 @@
+//
+//  RandomFunction.swift
+//  game4
+//
+//  Created by Facebook on 7/28/16.
+//  Copyright © 2016 DaryaVer. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+public extension CGFloat{
+    
+    
+    public static func random() -> CGFloat{
+        
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+        
+    }
+    
+    public static func random(min min : CGFloat, max : CGFloat) -> CGFloat{
+        
+        return CGFloat.random() * (max - min) + min
+    }
+    
+}
